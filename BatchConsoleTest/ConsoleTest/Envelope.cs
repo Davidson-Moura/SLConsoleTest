d9301da2259744c1053d3d1e62faff902fa38a9f
davidson moura <misterdavidson.moura@hotmail.com>
2024-08-05 13:02:46 -0300

    Init

---
+++ BatchConsoleTest/ConsoleTest/Envelope.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Xml.Serialization;
+
+namespace ConsoleTest
+{
+    [XmlType("Body")]
+    public class Body
+    {
+        [System.Xml.Serialization.XmlElement("JournalEntryBulkCreateRequest", Namespace = "http://sap.com/xi/SAPSCORE/SFIN")]
+        public JournalEntryCreateRequestBulkMessage Object { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Envelope", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+    public class Envelope<T> where T : new()
+    {
+        public Envelope()
+        {
+            
+        }
+        public Envelope(T obj)
+        {
+            Body = obj;
+        }
+
+        [XmlElement(ElementName = "Header")]
+        public BusinessDocumentMessageHeader Header { get; set; }
+
+        [XmlElement(ElementName = "Body")]
+        public T Body { get; set; }
+    }
+
+
+}
